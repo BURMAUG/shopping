e04cea97aa86d8fe7fad95978dd3274f2c53b3df
Burmau <burmaud1@gmail.com>
2023-10-01 14:27:47 -0400

    more has been done

--- shop/src/main/java/com/burmau/shop/pepper/Pepper.java
+++ shop/src/main/java/com/burmau/shop/pepper/Pepper.java
@@ -1,4 +1,22 @@
 package com.burmau.shop.pepper;
 
-public class Pepper {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@AllArgsConstructor @NoArgsConstructor
+@Getter @Setter
+@ToString
+@EqualsAndHashCode
+class Pepper {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long pepperID;
+    String description;
+    BigDecimal price;
 }
--- shop/src/main/java/com/burmau/shop/rice/Rice.java
+++ shop/src/main/java/com/burmau/shop/rice/Rice.java
@@ -1,4 +1,21 @@
 package com.burmau.shop.rice;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 class Rice {
+    @Id
+    Long riceID;
+    String description;
+    BigDecimal price;
 }
