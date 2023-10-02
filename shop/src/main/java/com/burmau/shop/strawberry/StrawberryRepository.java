e646065da1a595317ce64a002e0c96d7248a5ae4
Burmau <burmaud1@gmail.com>
2023-10-02 15:00:33 -0400

    Adding more to the pepper item

--- shop/src/main/java/com/burmau/shop/strawberry/StrawberryRepository.java
+++ shop/src/main/java/com/burmau/shop/strawberry/StrawberryRepository.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 
 public interface StrawberryRepository extends JpaRepository<Strawberry, Long> {
     void deleteID(Long id);
-
     Iterable<Strawberry> findBrand(String name);
 
     Optional<Strawberry> findID(Long id);
